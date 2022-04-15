0ab0db3874dcd1d27070d212e0c832f6f1af3eb1
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-04-15 18:22:14 +0300

    Delete deposit_calculator.py

--- deposit_calculator.py
+++
@@ -1,5 +0,0 @@
-deposit_sum = float(input())
-months_for_deposit = int(input())
-interest_rate = float(input())
-total_sum = deposit_sum + months_for_deposit*((deposit_sum*interest_rate/100)/12)
-print(total_sum)
