de7a217ffbce06893bcc4047432696a7295e5799
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-04-16 17:30:04 +0300

    Update and rename bonus_score to bonus_score.js

---
+++ conditional_statements/bonus_score.js
@@ -0,0 +1,21 @@
+function bonusScore (input) {
+    let points = Number(input[0]);
+    let bonus = 0;
+
+    if (points <= 100) {
+        bonus = 5;
+    } else if (points > 1000) {
+        bonus = 0.1 * points;
+    } else {
+        bonus = 0.2 * points;
+    }
+    if (points % 2 === 0) {
+        bonus  += 1;
+    }
+    if (points % 10 === 5) {
+        bonus +=  2;
+    }
+    console.log(bonus);
+    console.log(bonus+points);
+}
+
