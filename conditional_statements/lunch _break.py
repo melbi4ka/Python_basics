c2d31eb5f06931983a34398f7eb248fa9a9b51ae
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-04-17 18:51:54 +0300

    Update lunch _break.py

--- conditional_statements/lunch _break.py
+++ conditional_statements/lunch _break.py
@@ -1,3 +1,12 @@
+from math import ceil
 name_serial = input()
-time_for_episode = int(input())
-time_for_rest = int(input())
+movie_time = int(input())
+rest_time = int(input())
+time_lunch = 1/8 * rest_time
+time_break = 1/4 * rest_time
+free_time = rest_time - time_lunch - time_break
+total_time = abs(free_time - movie_time)
+if free_time > movie_time:
+    print(f"You have enough time to watch {name_serial} and left with {ceil(total_time)} minutes free time.")
+else:
+    print(f"You don't have enough time to watch {name_serial}, you need {ceil(total_time)} more minutes.")
