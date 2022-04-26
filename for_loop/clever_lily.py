b16c2378a2b8bb5b58ea2980f78eb7257ad162f9
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-04-26 10:07:03 +0300

    Add files via upload

---
+++ for_loop/clever_lily.py
@@ -0,0 +1,26 @@
+age = int(input())
+laundry_price = float(input())
+toy_price = int(input())
+money = 0
+toy = 0
+rest_money = 0
+
+for number in range(1, age+1):
+    if number % 2 == 0:
+        money = money + 10
+        rest_money += money - 1
+    else:
+        toy += 1
+
+total_sum = rest_money + toy * toy_price
+dif = abs(total_sum - laundry_price)
+if total_sum >= laundry_price:
+    print(f"Yes! {dif:.2f}")
+else:
+    print(f"No! {dif:.2f}")
+
+
+
+
+
+
