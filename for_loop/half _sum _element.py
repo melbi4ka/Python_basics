b2c55dbb004c1740fc7d842e3735b654a51466ec
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-04-26 10:07:32 +0300

    Add files via upload

---
+++ for_loop/half _sum _element.py
@@ -0,0 +1,22 @@
+import sys
+
+count_num = int(input())
+max_num = - sys.maxsize
+number_sum = 0
+for numbers in range(count_num):
+    numeral = int(input())
+    if numeral > max_num:
+        max_num = numeral
+    number_sum += numeral
+if max_num == number_sum - max_num:
+    print("Yes")
+    print(f"Sum = {max_num}")
+else:
+    print("No")
+    sum_others = number_sum - max_num
+    print(f"Diff = {abs(max_num - sum_others)}")
+
+
+
+
+
