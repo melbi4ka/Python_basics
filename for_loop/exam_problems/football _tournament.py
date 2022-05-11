9521811d4d7bc2fad0fa562cf8193cf13575db59
Maya Bachvarova <103676666+melbi4ka@users.noreply.github.com>
2022-05-11 11:34:26 +0300

    Add files via upload

---
+++ for_loop/exam_problems/football _tournament.py
@@ -0,0 +1,44 @@
+name = input()
+games = int(input())
+is_play = False
+points = 0
+w_counter = 0
+d_counter = 0
+l_counter = 0
+
+
+for numbers in range(1, games+1):
+    result = input()
+    if numbers < 1:
+        is_play = False
+    if numbers >= 1:
+        is_play = True
+        if result == "W":
+            points += 3
+            w_counter += 1
+        elif result == "D":
+            points += 1
+            d_counter += 1
+        elif result == "L":
+            points += 0
+            l_counter += 1
+all_counter = w_counter + d_counter + l_counter
+if is_play:
+    print(f"{name} has won {points} points during this season.")
+    print("Total stats:")
+    print(f"## W: {w_counter}")
+    print(f"## D: {d_counter}")
+    print(f"## L: {l_counter}")
+    print(f"Win rate: {w_counter / all_counter * 100:.2f}%")
+else:
+    print(f"{name} hasn't played any games during this season.")
+
+
+
+
+
+
+
+
+
+
